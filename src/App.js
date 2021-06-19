613a664746c84231c28431af5097fd18d5f527a2
ecabigting <ericcabigting@outlook.com>
2021-06-19 15:20:07 +0400

    some small errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Route  } from "react-router-dom";
-import Home from "./components/Home";
 import Photo from "./components/Photo"
 import './App.css';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <BrowserRouter>
       <div>
-        {/* <Route component={Home} path="/" exact /> */}
         <Route component={Photo} path="/" exact />
       </div>
     </BrowserRouter>
